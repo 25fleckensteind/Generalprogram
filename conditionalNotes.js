f02e519a4db6b0919c1e0f4087910666a8b76085
Fleckenstein <25FleckensteinD@badinhs.org>
2025-01-23 14:04:07 -0500

    taking notes on conditional

--- conditionalNotes.js
+++ conditionalNotes.js
@@ -1,1 +1,66 @@
 // These are notes on conditionals
+
+/* At its most basic level, a conditional statement is just "IF -> then"
+we will see many more complicated examples, but all of them rely on the idea 
+that you are checking if a condition is true, and then doing something 
+based on the true/false nature of that condition*/
+
+// Conditional statements rely on some form of comparison.
+
+/* Comparisons in javaScript, and most if not all programming languages 
+look like math equations to some extent.
+a < b (is a less than b?)
+a > b (is a greater than b?)
+a == b (is a equal b?)
+a <= b (is a less than or equal to b?)
+a >= b (is a greater than or equal to b?)
+*/
+
+// Example:
+// let a = 5;
+// console.log(5 < 3);
+// console.log(5 > 3);
+// console.log(a == 5);
+// console.log(a == "5");
+
+// javaScript also has a "===", for "strict equality". This means that 
+// it will ask "Is a equal to b AND do a and b have the same data type?"
+
+// console.log(a === "5");
+
+// the last symbol we need to talk about before jumping into actual conditionals
+// is "!". "!" means "NOT"
+// console.log(a != 3);
+
+// you can compare strings just like you can compare wiht numbers
+// console.log("Badin" > "Ross");
+/* Rules for string comparison: (assuming > for our comparison)
+1. compare the first characters of each string
+If the first character of the first string is greater than the first character
+of the second string, return True. If it is less than the first character of the 
+second string, return False. If they are equal, continue to next step.
+2. compare the second characters of each string, as described above
+3. continue until either string ends
+4. If both strings are equal AND have the same length, return false (they are equal)
+5. If one string is longer, that string is greater than the other string
+*/
+// console.log("R" > "B");
+// console.log("b" == "B");
+
+/* Now what we know how comparisons work, we can look at conditonals. */
+// AN if statement allows you to "gate" code behind a condition being tru.
+if (3 < 5){
+    console.log("3 is less than 5");
+}
+
+//If you want to make a choice between opitions, you have other structures that 
+//are useful.
+if (7 < 5){
+    console.log("7 is less than 5");
+}else{
+    console.log("7 is NOT less than 5");
+}
+
+// a test for equality.
+let testNumber = 6;
+if(testNUmber > )
--- dataTypeNotes.js
+++ dataTypeNotes.js
@@ -40,3 +40,30 @@ console.log(`Hello, ${myName}`);
 
 // you can also do math or other operations inside of curly braces:
 console.log(`2 + 3 + 23 = ${2 + 3 + 23}`);
+
+// Many programming lanuages have a "char" datatype, representing
+// a single character. JavaScript does not.
+
+/* Boolean
+Boolean is named for George Boole, who did a lot of work with formal logic
+namely, ways to make decisions using true and false statements. The Boolean
+datatype can hold only 2 values: true OR false.*/
+
+/* Null
+This is a special datatype. It contains only one value: Null.
+"Null" in javaScript means "nothing", or "value unknown", "empty"
+
+"Undefined"
+This is another special datatype. It contians only one value:
+undefined. "undefine" in javaScript means that the value has not
+been assigned
+
+let age:  <- here a variable was declared but not defined, so it is "undefined".
+
+our last two datatypes are Objects and Symbols, which are related, 
+and are likely beyond the scope of this class.
+
+If you need to know the type of a variable, you can use the typef() function */
+
+console.log(typeof("word"));
+console.log(typeof(356));
